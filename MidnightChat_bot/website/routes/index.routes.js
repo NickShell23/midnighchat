6acb691bfb642d3769b5bdb1f947746e35f47f0b
Mykyta Yevtushevskyi <mykyta.yevtushevskyi@epitech.eu>
2021-11-22 15:16:48 +0100

    Did it!

--- MidnightChat_bot/website/routes/index.routes.js
+++ MidnightChat_bot/website/routes/index.routes.js
@@ -2,14 +2,22 @@ const express = require('express')
 const router = express.Router()
 const adsController = require('../controllers/ads.controller')
 const models = require('../../db/models');
+const { render } = require('pug');
 
 router.get('/', function(req, res, next) {
-    models.users.findAll().then(function(user) {
+    return res.render('index', {
+      title: 'list of ads',
+    });
+    
+
+    
+
+/*     models.users.findAll().then(function(user) {
       res.render('index', {
         title: 'list of ads',
         user: user
       });
-    });
+    }); */
 });
 
 
